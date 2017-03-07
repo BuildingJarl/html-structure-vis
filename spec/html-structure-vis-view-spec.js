b5cbd94a3b803805544c3373d1c5a8e033992184
BuildingJarl <ivanbacher@socs-MacBook-Air-2.local>
2017-03-07 13:04:35 +0000

    save

---
+++ spec/html-structure-vis-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import HtmlStructureVisView from '../lib/html-structure-vis-view';
+
+describe('HtmlStructureVisView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
